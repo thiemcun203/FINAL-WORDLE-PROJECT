a4ff4961a534dbb3428e20974e56d8a159ed871e
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-01-03 00:48:42 +0700

    mmm

--- Interface.py
+++ Interface.py
@@ -1,8 +1,8 @@
 Ask=input('Choose interface mode:\n1.WordleBot\n2.Game with support\nElse to stop\n')
 while Ask.isdigit():
-    if Ask==1:
+    if Ask=='1':
         import WordleBot
-    else:
+    if Ask=='2':
         import Gamewithsupport
     Ask=input('Choose interface mode:\n1.WordleBot\n2.Game with support\nElse to stop\n')
        
