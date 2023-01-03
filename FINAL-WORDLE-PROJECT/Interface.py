586f7e93d2aabcf3e642c9a641f604774371f0ab
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-01-03 13:57:48 +0700

    Delete FINAL-WORDLE-PROJECT directory

--- FINAL-WORDLE-PROJECT/Interface.py
+++
@@ -1,7 +0,0 @@
-Ask=input('Choose interface mode:\n1.WordleBot\n2.Game with support\nElse to stop\n')
-while Ask.isdigit():
-    if Ask=='1':
-        import INTERFACE.WordleBot as WordleBot
-    if Ask=='2':
-        import INTERFACE.Gamewithsupport as Gamewithsupport
-    Ask=input('Choose interface mode:\n1.WordleBot\n2.Game with support\nElse to stop\n')
